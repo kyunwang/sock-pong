3a32865b3a02ff41fcf9a08ae72b140c7dc136fb
Kangyun Wang <kevinwang8011@hotmail.com>
2019-08-30 13:33:48 +0200

    GameRoom to WaitRoom

--- client/src/pages/room.js
+++ client/src/pages/room.js
@@ -2,7 +2,7 @@ import React from 'react';
 // import { Link } from 'gatsby';
 
 import Container from '../components/general/Container';
-import GameRoom from '../modules/WaitRoom/WaitRoom';
+import WaitRoom from '../modules/WaitRoom/WaitRoom';
 
 const WaitRoomPage = () => (
   <Container>
@@ -10,7 +10,7 @@ const WaitRoomPage = () => (
       <Link to="/play">play</Link>
 			{roomID} */}
 
-    <GameRoom />
+    <WaitRoom />
   </Container>
 );
 
