27553347d46ef962c9abfde718f97d986c605ae0
Kangyun Wang <kevinwang8011@hotmail.com>
2019-08-30 16:30:19 +0200

    Correction updating arr of coordinates

--- client/src/modules/PlayRoom/Client/Client.js
+++ client/src/modules/PlayRoom/Client/Client.js
@@ -6,27 +6,35 @@ import { subscribeToReceiveOrientation } from '../../../socket/socketSubscriptio
 
 const PlayRoomClient = ({ socket }) => {
   const { players } = useContext(GameContext);
+  const [coords, setCoords] = useState([null, null]);
 
-  const [alpha, setAlpha] = useState(0);
+  const handleSub = data => {
+    // orienation, playerID
+    const playerIndex = players.indexOf(data.playerID); // should be only 0 or 1
+    setCoords(oldArr => [
+      ...oldArr.slice(0, playerIndex),
+      data.orientation,
+      ...oldArr.slice(playerIndex + 1, coords.length),
+    ]);
+  };
 
   useEffect(() => {
-    subscribeToReceiveOrientation(socket, data => {
-      console.log('orientation: ', data.orientation);
-      setAlpha(data.orientation.alpha);
-    });
+    subscribeToReceiveOrientation(socket, handleSub);
   }, []);
 
   return (
-    <p>
+    <>
       <span>pr client</span>
       <ul>
-        {players.map(id => (
-          <li key={id}>
-            {id} - {alpha}
-          </li>
-        ))}
+        {players.length &&
+          coords.length &&
+          players.map((id, index) => (
+            <li key={id}>
+              {id} - {coords[index] && coords[index].alpha}
+            </li>
+          ))}
       </ul>
-    </p>
+    </>
   );
 };
 
